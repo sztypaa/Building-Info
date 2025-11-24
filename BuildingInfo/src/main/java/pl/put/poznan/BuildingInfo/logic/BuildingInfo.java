34417c2ee678f97d1da0364b584b434ff9c67eeb
kardach <162361263+kardach@users.noreply.github.com>
2025-11-24 17:11:43 +0100

    saving and retrieving location by its id (#20)

--- BuildingInfo/src/main/java/pl/put/poznan/BuildingInfo/logic/BuildingInfo.java
+++ BuildingInfo/src/main/java/pl/put/poznan/BuildingInfo/logic/BuildingInfo.java
@@ -4,23 +4,47 @@ import pl.put.poznan.BuildingInfo.model.*;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.stream.Collectors;
+
 /**
  * This is just an example to show that the logic should be outside the REST service.
  */
 public class BuildingInfo {
 
-    private final List<CompoundLocation> buildings;
+    private final List<Location> locations;
 
     public BuildingInfo(){
-        this.buildings = new ArrayList<>();
+        this.locations = new ArrayList<>();
     }
 
-    public void save(CompoundLocation building){
-        buildings.add(building);
+    public void save(Location location) {
+        locations.add(location);
     }
 
-    public CompoundLocation select() {
-        return buildings.get(0);
+    private Location getLocationById(Location location, int id) {
+        if(location.getId() == id) {
+            return location;
+        } else {
+           if (location instanceof CompoundLocation compoundLocation) {
+                for (Location sublocation : compoundLocation.getChildren()) {
+                    Location temp = getLocationById(sublocation, id);
+                    if(temp != null) {
+                        return temp;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    public Location getLocationById(int id) {
+        for(Location location : locations) {
+            Location temp = getLocationById(location, id);
+            if(temp != null) {
+                return temp;
+            }
+        }
+        return null;
     }
 
 }
--- BuildingInfo/src/main/java/pl/put/poznan/BuildingInfo/rest/BuildingInfoController.java
+++ BuildingInfo/src/main/java/pl/put/poznan/BuildingInfo/rest/BuildingInfoController.java
@@ -2,9 +2,11 @@ package pl.put.poznan.BuildingInfo.rest;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+import jakarta.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.BuildingInfo.logic.BuildingInfo;
 import pl.put.poznan.BuildingInfo.model.Location;
 import pl.put.poznan.BuildingInfo.other.LocationDeserializer;
 
@@ -12,15 +14,22 @@ import pl.put.poznan.BuildingInfo.other.LocationDeserializer;
 @RestController
 @RequestMapping("/")
 public class BuildingInfoController {
+    private final BuildingInfo buildingInfo = new BuildingInfo();
+    private final ObjectMapper mapper = new ObjectMapper();
+    private final SimpleModule module = new SimpleModule();
+
+    @PostConstruct
+    public void initialize() {
+        module.addDeserializer(Location.class, new LocationDeserializer());
+        mapper.registerModule(module);
+    }
 
     @PostMapping(value = "createBuilding", consumes = "application/json")
     public String createBuilding(@RequestBody ObjectNode json) {
-        final ObjectMapper mapper = new ObjectMapper();
-        final SimpleModule module = new SimpleModule();
-        module.addDeserializer(Location.class, new LocationDeserializer());
-        mapper.registerModule(module);
-
         Location location = mapper.convertValue(json, Location.class);
+
+        buildingInfo.save(location);
+
         return location.printAll();
     }
 
