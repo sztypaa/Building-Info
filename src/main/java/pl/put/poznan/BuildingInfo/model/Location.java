f4050f48b5c1cb76bd3bc714f009027009700f08
Pawel Brzozowski <pbrzozowski03@gmail.com>
2025-12-13 17:23:29 +0100

    Implement SLF4J logging (INFO/DEBUG) for code analyzability

--- src/main/java/pl/put/poznan/BuildingInfo/model/Location.java
+++ src/main/java/pl/put/poznan/BuildingInfo/model/Location.java
@@ -3,12 +3,17 @@ package pl.put.poznan.BuildingInfo.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonView;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import pl.put.poznan.BuildingInfo.other.LocationView;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
 @JsonIgnoreProperties(value={ "id", "volume" }, ignoreUnknown = true, allowGetters = true)
 public abstract class Location {
+
+    private static final Logger logger = LoggerFactory.getLogger(Location.class);
+
     protected static final AtomicInteger counter = new AtomicInteger(0);
     protected int id;
     protected String name;
@@ -47,6 +52,7 @@ public abstract class Location {
     @JsonProperty("average heating energy")
     @JsonView(LocationView.Heating.class)
     public float calculateHeatingEnergy() {
+        logger.debug("Calculating heating energy for location ID: {}", id);
         if (getCube() == 0) {
             return 0;
         }
@@ -58,6 +64,7 @@ public abstract class Location {
     @JsonProperty("average lighting power")
     @JsonView(LocationView.Lighting.class)
     public float calculateLightingPower() {
+        logger.debug("Calculating lighting power for location ID: {}", id);
         if (getArea() == 0) {
             return 0f;
         }
--- src/main/java/pl/put/poznan/BuildingInfo/rest/BuildingInfoController.java
+++ src/main/java/pl/put/poznan/BuildingInfo/rest/BuildingInfoController.java
@@ -1,9 +1,12 @@
 package pl.put.poznan.BuildingInfo.rest;
+
 import com.fasterxml.jackson.annotation.JsonView;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +18,9 @@ import pl.put.poznan.BuildingInfo.other.LocationView;
 @RestController
 @RequestMapping("/")
 public class BuildingInfoController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BuildingInfoController.class);
+
     private final BuildingInfo buildingInfo = new BuildingInfo();
     private final ObjectMapper mapper = new ObjectMapper();
     private final SimpleModule module = new SimpleModule();
@@ -23,15 +29,19 @@ public class BuildingInfoController {
     public void initialize() {
         module.addDeserializer(Location.class, new LocationDeserializer());
         mapper.registerModule(module);
+        logger.debug("BuildingInfoController initialized");
     }
 
     @JsonView(LocationView.All.class)
     @PostMapping(value = "createBuilding", consumes = "application/json", produces = "application/json")
     @ResponseBody
     public ResponseEntity<Location> createBuilding(@RequestBody ObjectNode json) {
-        Location location = mapper.convertValue(json, Location.class);
+        logger.info("Received POST request to create building");
 
+        Location location = mapper.convertValue(json, Location.class);
         buildingInfo.save(location);
+
+        logger.debug("Building structure created successfully with root ID: {}", location.getId());
 
         return new ResponseEntity<>(location, HttpStatus.OK);
     }
@@ -40,21 +50,42 @@ public class BuildingInfoController {
     @GetMapping(value = "getAll", produces = "application/json")
     @ResponseBody
     public ResponseEntity<Location> getAll(@RequestParam int id) {
+        logger.info("Received GET request: getAll for ID {}", id);
+
         Location location = buildingInfo.getLocationById(id);
+        if (location == null) {
+            logger.error("Location with ID {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return new ResponseEntity<>(location, HttpStatus.OK);
     }
 
     @GetMapping(value = "print", produces = "text/plain")
     @ResponseBody
     public ResponseEntity<String> print(@RequestParam int id) {
+        logger.info("Received GET request: print for ID {}", id);
+
         Location location = buildingInfo.getLocationById(id);
+        if (location == null) {
+            logger.error("Location with ID {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return new ResponseEntity<>(location.print(), HttpStatus.OK);
     }
 
     @GetMapping(value = "printAll", produces = "text/plain")
     @ResponseBody
     public ResponseEntity<String> printAll(@RequestParam int id) {
+        logger.info("Received GET request: printAll for ID {}", id);
+
         Location location = buildingInfo.getLocationById(id);
+        if (location == null) {
+            logger.error("Location with ID {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
         return new ResponseEntity<>(location.printAll(), HttpStatus.OK);
     }
 
@@ -62,31 +93,67 @@ public class BuildingInfoController {
     @GetMapping(value = "getCube", produces = "application/json")
     @ResponseBody
     public ResponseEntity<Location> getCube(@RequestParam int id) {
-        return new ResponseEntity<>(buildingInfo.getLocationById(id), HttpStatus.OK);
+        logger.info("Received GET request: getCube for ID {}", id);
+
+        Location location = buildingInfo.getLocationById(id);
+        if (location == null) {
+            logger.error("Location with ID {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        logger.debug("Returning cube for location ID {}: {}", id, location.getCube());
+        return new ResponseEntity<>(location, HttpStatus.OK);
     }
 
     @JsonView(LocationView.Area.class)
     @GetMapping(value = "getArea", produces = "application/json")
     @ResponseBody
     public ResponseEntity<Location> getArea(@RequestParam int id) {
-        return new ResponseEntity<>(buildingInfo.getLocationById(id), HttpStatus.OK);
-    }
+        logger.info("Received GET request: getArea for ID {}", id);
 
+        Location location = buildingInfo.getLocationById(id);
+        if (location == null) {
+            logger.error("Location with ID {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        logger.debug("Returning area for location ID {}: {}", id, location.getArea());
+        return new ResponseEntity<>(location, HttpStatus.OK);
+    }
 
     @JsonView(LocationView.Heating.class)
     @GetMapping(value = "calculateHeating", produces = "application/json")
     @ResponseBody
     public ResponseEntity<Location> calculateHeating(@RequestParam int id) {
-        return new ResponseEntity<>(buildingInfo.getLocationById(id), HttpStatus.OK);
+        logger.info("Received GET request: calculateHeating for ID {}", id);
+
+        Location location = buildingInfo.getLocationById(id);
+        if (location == null) {
+            logger.error("Location with ID {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        float result = location.calculateHeatingEnergy();
+        logger.debug("Heating energy calculated for ID {}: {}", id, result);
+
+        return new ResponseEntity<>(location, HttpStatus.OK);
     }
 
     @JsonView(LocationView.Lighting.class)
     @GetMapping(value = "calculateLighting")
     @ResponseBody
     public ResponseEntity<Location> calculateLighting(@RequestParam int id) {
-        return new ResponseEntity<>(buildingInfo.getLocationById(id), HttpStatus.OK);
+        logger.info("Received GET request: calculateLighting for ID {}", id);
+
+        Location location = buildingInfo.getLocationById(id);
+        if (location == null) {
+            logger.error("Location with ID {} not found", id);
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        float result = location.calculateLightingPower();
+        logger.debug("Lighting power calculated for ID {}: {}", id, result);
+
+        return new ResponseEntity<>(location, HttpStatus.OK);
     }
-
 }
-
-
