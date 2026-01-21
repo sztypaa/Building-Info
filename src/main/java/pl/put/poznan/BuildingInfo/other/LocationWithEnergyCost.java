10a22f3b988082b771ee32498ea509604d154a92
Pawel Brzozowski <pbrzozowski03@gmail.com>
2026-01-21 17:43:23 +0100

    Restored LocationWithEnergyCost implementation

--- src/main/java/pl/put/poznan/BuildingInfo/other/LocationWithEnergyCost.java
+++ src/main/java/pl/put/poznan/BuildingInfo/other/LocationWithEnergyCost.java
@@ -1,21 +1,9 @@
 package pl.put.poznan.BuildingInfo.other;
 
+import com.fasterxml.jackson.annotation.JsonUnwrapped;
+import com.fasterxml.jackson.annotation.JsonView;
 import pl.put.poznan.BuildingInfo.model.Location;
 
-public class LocationWithEnergyCost {
-    private final Location location;
-    private final double cost;
-
-    public LocationWithEnergyCost(Location location, double cost) {
-        this.location = location;
-        this.cost = cost;
-    }
-
-    public Location getLocation() {
-        return location;
-    }
-
-    public double getCost() {
-        return cost;
-    }
+@JsonView(LocationView.EnergyCost.class)
+public record LocationWithEnergyCost(@JsonUnwrapped Location location, float energyCost) {
 }
