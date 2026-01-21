ae2001815700c92af21148c12a7bcc84872d72da
PawelB03 <78073907+PawelB03@users.noreply.github.com>
2026-01-22 00:24:13 +0100

    restored deleted javadoc comment
    Add Javadoc documentation for LocationWithEnergyCost class

--- src/main/java/pl/put/poznan/BuildingInfo/other/LocationWithEnergyCost.java
+++ src/main/java/pl/put/poznan/BuildingInfo/other/LocationWithEnergyCost.java
@@ -1,5 +1,12 @@
 package pl.put.poznan.BuildingInfo.other;
 
+/**
+ * <code>LocationWithEnergyCost</code> is a wrapper class of <code>{@link Location}</code> used in
+ * <code>{@link pl.put.poznan.BuildingInfo.rest.BuildingInfoController#calculateEnergyCost(int)}</code>.
+ *
+ * @version %I% %D%
+ */
+
 import com.fasterxml.jackson.annotation.JsonUnwrapped;
 import com.fasterxml.jackson.annotation.JsonView;
 import pl.put.poznan.BuildingInfo.model.Location;
